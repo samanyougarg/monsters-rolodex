3a85e6b3cccf713929410881aacecefad67e3a4f
Sam <samygarg@deloitte.co.uk>
2019-10-27 12:03:48 +0000

    Updates

---
+++ precache-manifest.adc3b978a4bd71dea96f74143b53e0a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353c9609a667ce4eaaad80a15cad5799",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "de62ac70f54573cf18f3",
+    "url": "/monsters-rolodex/static/css/main.263c55b5.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "de62ac70f54573cf18f3",
+    "url": "/monsters-rolodex/static/js/main.053a2583.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
